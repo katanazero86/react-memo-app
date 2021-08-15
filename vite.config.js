a839300bd256d1d172db554d0455470d81c6b72e
z1004man@naver.com <z1004man@naver.com>
2021-08-16 04:09:10 +0900

    [feature] 프로젝트 생성 - react 프로젝트 생성

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
+import path from 'path';
+import fs from 'fs';
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+    css: {
+        modules: {localsConvention: 'camelCase'},
+        generateScopedName: '[name]_[hash:base64:5]'
+    },
+    resolve: {
+        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+        alias: {
+            '@': path.resolve(__dirname, '/src'),
+        },
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 5000,
+        https: {
+            key: fs.readFileSync(path.resolve(__dirname, './localhost-key.pem')),
+            cert: fs.readFileSync(path.resolve(__dirname, './localhost.pem')),
+        }
+    },
+    plugins: [reactRefresh()]
 })
