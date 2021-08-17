c21c7d54ade7b7a4db466cf7f5cd92b39d342c5e
z1004man@naver.com <z1004man@naver.com>
2021-08-18 05:53:36 +0900

    [feature] 메모 관련 컴포넌트 작성 - 라우터 레이아웃 코드 작성 - 인풋 컴포넌트 작성

--- src/store/atoms.js
+++ src/store/atoms.js
@@ -1,3 +1,36 @@
 import { atom } from 'jotai'
 
 export const countAtom = atom(0);
+
+export const memoLabels = atom([
+    {
+        id: 'label-1',
+        hex: '#21ba45',
+        rgb: '33,186,69',
+        name: '메모'
+    },
+    {
+        id: 'label-2',
+        hex: '#c10015',
+        rgb: '93,0,21',
+        name: '중요'
+    },
+    {
+        id: 'label-3',
+        hex: '#e6e6e6',
+        rgb: '230,230,230',
+        name: '주의'
+    },
+    {
+        id: 'label-4',
+        hex: '#7a69ff',
+        rgb: '122,105,255',
+        name: '프론트엔드',
+    },
+    {
+        id: 'label-5',
+        hex: '#4da5bd',
+        rgb: '77,165,189',
+        name: '백엔드',
+    },
+])
